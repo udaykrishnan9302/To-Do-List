e44a4911c0e9c4390c95ac1bb86874111c49ea27
Uday Krishnan <udaykrishnanvg@gmail.com>
2022-07-24 02:17:18 +0530

    Final changes, proper numbering system

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import TskAdd from './component/addTask';
 import Todo from './component/taskComp';
-
+var count=1;
 function App() {
+
   const [tasks, setTasks] = React.useState([
     {
-      data:"",
-    },
+      data:""
+    }
   ]);
 
   const addTask = (data) => {
-    
-    const nwTsk = [...tasks, { data }];
+    tasks.length==1 ?count=1:count=count+1;
+    const nwTsk = [...tasks, { data:` ${data}` }];
     setTasks(nwTsk);
   };
 
@@ -19,10 +20,11 @@ function App() {
     const nwTsk = [...tasks];
     nwTsk.splice(index, 1);
     setTasks(nwTsk);
+    console.log(index);
   };
 
   return (
-    <div className="flex justify-center p-52">
+    <div className="flex justify-center p-8">
       <div className=" p-10 text-center bg-cyan-50 shadow-xl rounded-lg" >
         <h1 className="text-5xl p-4" >To Do List</h1>
         <TskAdd addTask={addTask} />
--- src/component/taskComp.js
+++ src/component/taskComp.js
@@ -4,7 +4,7 @@ function Todo({ todo, index, delTask }) {
     <div className="p-1">
       {todo.data !=="" ? <div className="flex px-2">
         <div className="">
-          <span className="text-2xl">{todo.data}</span>
+          <span className="text-2xl">{index}.{todo.data}</span>
         </div>
         <div className="ml-auto">
          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded ml-2" onClick={() => delTask(index)}>X</button>
