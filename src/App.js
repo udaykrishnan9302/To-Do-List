f5be1e478fb6cc96a42f210687c3287661d1e3ca
Uday Krishnan <udaykrishnanvg@gmail.com>
2022-07-26 23:31:39 +0530

    Implemented Backend using Firebase

--- src/App.js
+++ src/App.js
@@ -1,46 +1,121 @@
 import React from "react";
-import TskAdd from './component/addTask';
-import Todo from './component/taskComp';
-var count=1;
+import { db } from "./firebase-config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [Task, setTask] = useState([{}]);
+  const TaskCollectionRef = collection(db, "TaskCollection");
 
-  const [tasks, setTasks] = React.useState([
-    {
-      data:""
-    }
-  ]);
-
-  const addTask = (data) => {
-    tasks.length==1 ?count=1:count=count+1;
-    const nwTsk = [...tasks, { data:` ${data}` }];
-    setTasks(nwTsk);
+  const createTask = async () => {
+    await addDoc(TaskCollectionRef, { Task: value });
   };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "TaskCollection", id);
+    await deleteDoc(userDoc);
+  };
+
+  useEffect(() => {
+    const getList = async () => {
+      const dat = await getDocs(TaskCollectionRef);
+      setCount(dat.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log({ TDetails });
+    };
+    getList();
+  }, []);
+
+  const [value, setValue] = React.useState("");
+  const [TDetails, setCount] = useState([{}]);
 
   const delTask = (index) => {
-    const nwTsk = [...tasks];
+    const nwTsk = [...TDetails];
     nwTsk.splice(index, 1);
-    setTasks(nwTsk);
-    console.log(index);
+    setCount(nwTsk);
   };
 
+  const addbutton = (e) => {
+    e.preventDefault();
+    const nwTsk = [...TDetails, { TaskVal: value }];
+    setTask(nwTsk);
+
+    if (!value) return;
+    var tskStore = [...TDetails, { Task: value }];
+    setCount(tskStore);
+    createTask();
+    setValue("");
+  };
+
+  /*DATA DISPLAYING AND DELETE BUTTON*/
+  function Todo({ todo, index, delTask }) {
+    return (
+      <div className="p-1">
+        {todo.Task !== "" ? (
+          <div className="flex px-2">
+            <span className="text-2xl">
+              {index + 1}.{todo.Task}
+            </span>
+
+            <div className="ml-auto">
+              <button
+                className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded ml-2"
+                onClick={() => {
+                  delTask(index);
+                  deleteUser(todo.id);
+                }}
+              >
+                X
+              </button>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  }
+  /*App func return */
   return (
     <div className="flex justify-center p-8">
-      <div className=" p-10 text-center bg-cyan-50 shadow-xl rounded-lg" >
-        <h1 className="text-5xl p-4" >To Do List</h1>
-        <TskAdd addTask={addTask} />
-       {tasks.length===1 ?
-       <div className="p-4">
-       {tasks.map((todo, index) => (
-         <Todo index={index} todo={todo} delTask={delTask} />
-       ))}
-       </div>
-       :
-       <div className="border-2 border-black mr-3 mt-3 rounded-lg">
-       {tasks.map((todo, index) => (
-         <Todo key={index} index={index} todo={todo} delTask={delTask} />
-       ))}
-       </div>
-      }
+      <div className=" p-10 text-center bg-cyan-50 shadow-xl rounded-lg">
+        <h1 className="text-5xl p-4">To Do List</h1>
+
+        <div class="p-3 text-3xl">
+          <b>ADD TASKS</b>
+        </div>
+
+        <input
+          type="text"
+          className="rounded-lg px-6 border-2 border-neutral-900 p-2"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Add new Tasks"
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded ml-2"
+          onClick={addbutton}
+        >
+          ADD
+        </button>
+        {TDetails.length === 0 ? (
+          <div className="p-4">
+            {TDetails.map((todo, index) => (
+              <Todo index={index} todo={todo} delTask={delTask} />
+            ))}
+          </div>
+        ) : (
+          <div className="border-2 border-black mr-3 mt-3 rounded-lg">
+            {TDetails.map((todo, index) => (
+              <Todo key={index} index={index} todo={todo} delTask={delTask} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtPmR1G-F6fgD5dOiIX7rsQiNv33oXAHg",
+    authDomain: "to-do-react-68a65.firebaseapp.com",
+    projectId: "to-do-react-68a65",
+    storageBucket: "to-do-react-68a65.appspot.com",
+    messagingSenderId: "1027583123842",
+    appId: "1:1027583123842:web:0a0712b73b648a0858af9b",
+    measurementId: "G-5T3RXKYD6X"
+  };
+  const app = initializeApp(firebaseConfig);
+  export const db = getFirestore(app);
